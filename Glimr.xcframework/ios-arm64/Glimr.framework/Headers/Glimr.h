1a5fbec09fc3d15f8400a6f54ddcca0c23e4e377
Sven Roeder <svenroed@gmail.com>
2021-12-15 21:14:37 +0100

    initial commit

---
+++ Glimr.xcframework/ios-arm64/Glimr.framework/Headers/Glimr.h
@@ -0,0 +1,18 @@
+//
+//  Glimr.h
+//  Glimr
+//
+//  Created by Werner Huber on 12.12.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Glimr.
+FOUNDATION_EXPORT double GlimrVersionNumber;
+
+//! Project version string for Glimr.
+FOUNDATION_EXPORT const unsigned char GlimrVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Glimr/PublicHeader.h>
+
+
---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.3
+import PackageDescription
+let package = Package(  
+    name: "GLGeoRealtime",    
+    defaultLocalization: "en",
+    platforms: [    
+        .iOS(.v9)   
+    ],  
+    products: [ 
+        // Products define the executables and libraries a package produces, and make them visible to other packages.   
+        .library(   
+            name: "GLGeoRealtime",    
+            targets: ["GLGeoRealtime"]),
+        .library(   
+            name: "Glimr Swift",    
+            targets: ["Glimr"])   
+    ],  
+    dependencies: [ 
+        // Dependencies declare other packages that this package depends on.    
+    ],  
+    targets: [  
+        .binaryTarget(  
+            name: "GLGeoRealtime",    
+            path: "GLGeoRealtimeStatic.xcframework"   
+        ),
+        .binaryTarget(  
+            name: "Glimr",    
+            path: "Glimr.xcframework" 
+        )       
+    ]   
+) 
